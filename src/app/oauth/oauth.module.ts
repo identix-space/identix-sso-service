d4cd9aa48c970991cb52c11313b158d3b96dc94f
uxname <uxname@gmail.com>
2023-04-28 13:32:25 +0300

    Added DID to accounts

--- src/app/oauth/oauth.module.ts
+++ src/app/oauth/oauth.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 
 import { AccountSessionModule } from '@/app/account-session/account-session.module';
+import { WalletsServiceClientModule } from '@/app/wallets-service-client/wallets-service-client.module';
 import { CryptoModule } from '@/common/crypto/crypto.module';
 import { PrismaModule } from '@/common/prisma/prisma.module';
 
@@ -8,7 +9,12 @@ import { OauthResolver } from './oauth.resolver';
 import { OauthService } from './oauth.service';
 
 @Module({
-  imports: [PrismaModule, CryptoModule, AccountSessionModule],
+  imports: [
+    PrismaModule,
+    CryptoModule,
+    AccountSessionModule,
+    WalletsServiceClientModule,
+  ],
   providers: [OauthResolver, OauthService],
   exports: [OauthService],
 })
